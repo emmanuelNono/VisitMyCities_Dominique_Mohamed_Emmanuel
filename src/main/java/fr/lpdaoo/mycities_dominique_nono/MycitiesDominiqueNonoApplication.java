604b0a1005ba63087e06308c9a9b3c6514499623
emnono <45090671+emnono@users.noreply.github.com>
2020-03-31 09:10:56 +0200

    first commit : mise en partage du repository

---
+++ src/main/java/fr/lpdaoo/mycities_dominique_nono/MycitiesDominiqueNonoApplication.java
@@ -0,0 +1,13 @@
+package fr.lpdaoo.mycities_dominique_nono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycitiesDominiqueNonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycitiesDominiqueNonoApplication.class, args);
+	}
+
+}
