a3d099caaf8422d5f7c474b4deb77e6e91f3303f
Florent.Bourgeois <florent.bourgeois@uha.fr>
2020-05-19 09:15:59 +0200

    patch sql file to match auto database creation (uppercases not in table names)

--- VisitMyCities-Batiment/src/main/resources/data.sql
+++ VisitMyCities-Batiment/src/main/resources/data.sql
@@ -8,7 +8,7 @@
 
 
 -- fillingf of the table Batiment
-INSERT INTO Batiment(bat_id, bat_nom, bat_adresse, bat_coord_gps_ns, bat_coord_gps_oe, bat_annee_construction, bat_couleur, bat_description, bat_architecte, bat_ville ) VALUES 
+INSERT INTO batiment(bat_id, bat_nom, bat_adresse, bat_coord_gps_ns, bat_coord_gps_oe, bat_annee_construction, bat_couleur, bat_description, bat_architecte, bat_ville ) VALUES
 (1, "Le Parlement Européen", "1 Avenue du Président Robert Schuman", "nord", "ouest", 1999, "transparent", "Un très bel édifice", "Architecture Studio", 1),
 (2, "La Mairie de Schiltigheim", "110 Route de Bischwiller, 67300 Schiltigheim", "nord", "ouest", 1969, "translucide", "Le nouvel hôtel de ville de Schiltigheim est construit de 1964 à 19691. Il est inauguré le 15 mars 19691. Les architectes en charge du projet sont alors Louis Schneider, de Strasbourg et Henri-Jean Calsat, de Paris1, ce dernier est également l'auteur de la Tour du square à Schiltigheim. De la même façon que la Tour du Square avait pris place sur l'emplacement d'une ancienne malterie, la nouvelle mairie est érigée sur le site de la malterie Lévy2, détruite en 1975 avec le Coq d'Or1 et la 'maison du directeur'. La nouvelle Mairie figure également au Plan de Rénovation du centre-ville de l'époque, dit 'Plan Calsat'1", "Louis Schneider", 2),
 (3, "Cathédrale Notre Dame de Strasbourg", "2 Place de la Cathédrale", "nord", "ouest", 1180, "Grés des vosges", "La cathédrale de Strasbourg avec sa flèche unique de 142 mètres est le plus haut monument et le plus célèbre de la ville. Sur un sol mou, argileux, on a enfoncé des pieux de bois puis un mur de fondations. Les édifices se succèdent, de plus en plus lourds, reprenant d’anciens tracés. Ces chantiers ont tous été parmi les plus remarquables de l’Empire Germanique tant par les dimensions des édifices que par la puissance de leurs architectures1. ", "Des architectes", 1),
@@ -16,7 +16,7 @@ INSERT INTO Batiment(bat_id, bat_nom, bat_adresse, bat_coord_gps_ns, bat_coord_g
 
 
 -- filling of the table Photo
-INSERT INTO Photo (pho_id, pho_titre, pho_source, batiment_bat_id) VALUES 
+INSERT INTO photo (pho_id, pho_titre, pho_source, batiment_bat_id) VALUES
 (1, "De l'extérieur", "https://www.challenges.fr/assets/img/2019/02/26/cover-r4x3w1000-5c9128900ad90-0ecd1063ad1bf594c59f7cdd03c93bda39684607-jpg.jpg",1),
 (2, "L'hémicycle", "https://www.google.fr/url?sa=i&url=https%3A%2F%2Fwww.touteleurope.eu%2Factualite%2Fle-parlement-europeen-2019-2024.html&psig=AOvVaw33VcHff-q4m2DtXA4TuLDv&ust=1587646220349000&source=images&cd=vfe&ved=0CA0QjhxqFwoTCLjls5yJ_OgCFQAAAAAdAAAAABAI",1),
 (3, "La fierté de l'Alsace :)", "https://commons.wikimedia.org/wiki/File:Strasbourg_Cathedral_Exterior_-_Diliff.jpg?uselang=fr",3),
